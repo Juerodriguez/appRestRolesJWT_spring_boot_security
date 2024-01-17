ba210c3f181ac3d7ace6f91c7536e7b6449fb8ab
Rodriguez Juan <stylorj@gmail.com>
2024-01-17 15:03:01 -0300

    First Commit

---
+++ src/main/java/com/juerodriguez/appRestRolesJWT/AppRestRolesJwtApplication.java
@@ -0,0 +1,13 @@
+package com.juerodriguez.appRestRolesJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestRolesJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestRolesJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juerodriguez/appRestRolesJWT/AppRestRolesJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juerodriguez.appRestRolesJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestRolesJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
